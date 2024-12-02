af10c42c215cbeb0b3df772d4fcea92b8047fe00
Salvador Ron <salvadorronosio@gmail.com>
2024-12-02 16:16:09 -0400

    updates

--- src/infrastructure/repositories/membership/membership.repository.ts
+++ src/infrastructure/repositories/membership/membership.repository.ts
@@ -8,15 +8,17 @@ export class MembershipRepositoryImpl implements MembershipRepository {
     constructor(private prisma: PrismaService) {}
     
     async save(data: Prisma.MembershipCreateInput): Promise<Membership> {
-        const membership = await this.prisma.membership.create({ data, include: { rate: true } });
+        const membership = await this.prisma.membership.create({ data, include: { rate: true, clients: true } });
         return membership;
     }
+    
     async getMemberships(): Promise<Membership[]> {
-        const memberships = await this.prisma.membership.findMany({ include: { rate: true } });
+        const memberships = await this.prisma.membership.findMany({ include: { rate: true, clients: true } });
         return memberships;
     }
+
     async getOneMembership(name: string, type: string): Promise<Membership> {
-        const membership = await this.prisma.membership.findFirst({ where: { name, type}, include: { rate: true } });
+        const membership = await this.prisma.membership.findFirst({ where: { name, type}, include: { rate: true, clients: true } });
         if(!membership) throw new HttpException('Membership not found', 404);
         return membership
     }
