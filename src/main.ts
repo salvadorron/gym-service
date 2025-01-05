9fa5261bf9ddde783cdb9518fd36c04c1ae37f8a
Salvador Ron <salvadorronosio@gmail.com>
2025-01-05 14:44:53 -0400

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
-    customSiteTitle: 'Backend Generator',
+    customSiteTitle: 'Gym Service',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
